06739324d1b2f62f2ae2847a9e315b07984b5e4d
kruglyakovv <148679681+23350406@users.noreply.github.com>
2024-11-02 17:15:49 +0300

    Update pythonReady.cpp

--- code/pythonReady.cpp
+++ code/pythonReady.cpp
@@ -1,1 +1,44 @@
+////////////////////////////////////////////////////////////
+// Headers
+////////////////////////////////////////////////////////////
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <cmath>
+#include <ctime>
+#include <cstdlib>
 
+int main()
+{
+	// Откроем окно для вывода
+	sf::RenderWindow window(sf::VideoMode(900, 600), "Python");
+
+	/*Создадим объект типа image и будем выгружать его в оконное приложение
+	  Важно создавать объект, т.к. есть свойства, которые не всегда найдутся в текстуре.
+	  К примеру маска цветов - можем игнорировать какие либо цвета объекта*/ 
+	sf::Image pythonImage;
+	pythonImage.loadFromFile("images/pngwing.com.png");
+
+	// Текстура и есть изображение. Можно не использовать Image (НО НЕЛЬЗЯ!!)
+	sf::Texture pythonTexture;
+	pythonTexture.loadFromImage(pythonImage);
+
+	sf::Sprite pythonSprite;
+	pythonSprite.setTexture(pythonTexture);
+	pythonSprite.setPosition(420, 25);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(pythonSprite);
+		window.display();
+	}
+
+	return 0;
+}
