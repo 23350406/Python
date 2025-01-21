327ec1e28a3b992fad7e27d0d3e50fc15216bbb0
frozzzen123 <148707584+frozzzen123@users.noreply.github.com>
2025-01-21 17:31:23 +0700

    Update GameInfo.h

--- Python/include/GameInfo.h
+++ Python/include/GameInfo.h
@@ -10,9 +10,32 @@ GameInfo::GameInfo() {
 	_numberOfBots = 0;
 	_mapSize = "medium";
 	_somethingIsPressed = false;
+	_isSolo = true;
 
 	// Задается название открытого сейчас окна.
 	_currentWindowName = "MainMenu";
+
+	// Создаются объекты для хранения настроек игроков.
+	_firstPlayerInfo = CreateFirstPlayer();
+	_secondPlayerInfo = CreateSecondPlayer();
+
+	// Для изменяемого поля устанавливается пустое значение т.к. никакой из полей не изменяется.
+	_fieldInUse = "";
+}
+
+// Функция устанавливает значение на игру для двоих.
+void GameInfo::SetDuo() {
+	_isSolo = false;
+}
+
+// Функция устанавливает значение на игру для одного.
+void GameInfo::SetSolo() {
+	_isSolo = true;
+}
+
+// Функция возвращает: сколько человек играет.
+bool GameInfo::GetIsSolo() {
+	return _isSolo;
 }
 
 // Функция возвращает количество раундов в игре.
@@ -53,11 +76,12 @@ void GameInfo::DecreaseNumberOfBots() {
 	}
 }
 
-//  TODO: Комментарии
+// Функция возвращает размер игрового поля.
 std::string GameInfo::GetMapSize() {
 	return _mapSize;
 }
 
+// Функция для того, чтобы сменить карту игры на большую.
 void GameInfo::IncreaseMapSize() {
 	if (_mapSize == "medium") {
 		_mapSize = "large";
@@ -67,7 +91,7 @@ void GameInfo::IncreaseMapSize() {
 	}
 }
 
-
+// Функция для того, чтобы сменить карту игры на меньшую.
 void GameInfo::DecreaseMapSize() {
 	if (_mapSize == "medium") {
 		_mapSize = "small";
@@ -77,24 +101,59 @@ void GameInfo::DecreaseMapSize() {
 	}
 }
 
+// Функция, отлавливающая нажатаю кнопку
 bool GameInfo::GetPressedButton() {
 	return _somethingIsPressed;
 }
 
+// Функция, устанавливающая кнопку по умолчанию
 void GameInfo::SetPressedButton() {
 	_somethingIsPressed = true;
 }
 
+// Функция отжимает кнопку в классе.
 void GameInfo::UnsetPressedButton() {
 	_somethingIsPressed = false;
 }
 
+// Функция возвращает название текущего экрана.
 std::string GameInfo::GetCurrentWindowName() {
 	return _currentWindowName;
 }
 
+// Функция устанавливает название текущего экрана.
 void GameInfo::SetCurrentWindowName(std::string newWindowName) {
 	_currentWindowName = newWindowName;
+}
+
+// Функция возвращает информацию о первом игроке.
+PlayerInfo GameInfo::GetFirstPlayerInfo() {
+	return _firstPlayerInfo;
+}
+
+// Функция устанавливает информацию о первом игроке.
+void GameInfo::SetFirstPlayerInfo(PlayerInfo newInfo) {
+	_firstPlayerInfo = newInfo;
+}
+
+// Функция возвращает информацию о втором игроке.
+PlayerInfo GameInfo::GetSecondPlayerInfo() {
+	return _secondPlayerInfo;
+}
+
+// Функция устанавливает информацию о втором игроке.
+void GameInfo::SetSecondPlayerInfo(PlayerInfo newInfo) {
+	_secondPlayerInfo = newInfo;
+}
+
+// Функция возвращает название изменяемого поля.
+std::string GameInfo::GetFieldInUse() {
+	return _fieldInUse;
+}
+
+// Функция устанавливает название изменяемого поля.
+void GameInfo::SetFieldInUse(std::string fieldName) {
+	_fieldInUse = fieldName;
 }
 
 #endif // GAMEINFO_HPP
